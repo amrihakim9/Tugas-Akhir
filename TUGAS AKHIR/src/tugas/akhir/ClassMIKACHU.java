f6a3b3b03b301f97dbaf0b0a11a85ef3ee338182
amrihakim9 <mamrihakim957@gmail.com>
2020-05-13 11:39:28 +0700

    Project TA DKP 2020
    Membuat Program Sederhana

---
+++ TUGAS AKHIR/src/tugas/akhir/ClassMIKACHU.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.akhir;
+
+/**
+ *
+ * @author DELL
+ */
+public class ClassMIKACHU {
+    
+    private String hasil;
+    
+    public void sethasil(String hasil){
+        this.hasil = hasil;
+    }
+        
+    public String gethasil(){
+        return hasil;
+    }
+}
